fc776f7a6fac52b451cf3be8153d1051f71f7f2c
alexlyxymobile@gmail.com <alexlyxymobile@gmail.com>
2023-09-01 16:44:16 +0300

    Initial commit

---
+++ app/src/main/java/com/alexlyxy/weather/DayItem.kt
@@ -0,0 +1,12 @@
+package com.alexlyxy.weather
+
+data class DayItem (
+    val city: String,
+    val time: String,
+    val condition: String,
+    val currentUrl: String,
+    val currentTemp: String,
+    val maxTemp: String,
+    val minTemp: String,
+    val hours: String
+)
