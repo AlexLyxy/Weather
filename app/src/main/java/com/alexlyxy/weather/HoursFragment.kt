ca3e5531f5fb6e666dd35a065adb6088400f430b
Oleksandr Lysenko <alexlyxy@gmail.com>
2023-09-04 20:55:11 +0300

    Last 04 09 2023

--- app/src/main/java/com/alexlyxy/weather/HoursFragment.kt
+++ app/src/main/java/com/alexlyxy/weather/HoursFragment.kt
@@ -5,16 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.alexlyxy.weather.adapters.WeatherAdapter
+import com.alexlyxy.weather.adapters.WeatherModel
+import com.alexlyxy.weather.databinding.FragmentHoursBinding
 
 class HoursFragment : Fragment() {
-
+    private lateinit var binding: FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
     override fun onCreateView(
-        inflater :LayoutInflater,container :ViewGroup?,
-        savedInstanceState :Bundle?,
-    ) :View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours,container,false)
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+    }
+
+    private fun initRcView() = with(binding) {
+        rcView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rcView.adapter = adapter
+        val list = listOf(
+            WeatherModel(
+                "", "12:00", "Sunny",
+                "25", "", "",
+                "", ""
+            ),
+            WeatherModel(
+                "", "15:00", "SunnySunny",
+                "30", "", "",
+                "", ""
+            ),
+            WeatherModel(
+                "", "18:00", "SunnyRain",
+                "35", "", "",
+                "", ""
+            )
+        )
+        adapter.submitList(list)
+    }
+
     companion object {
         @JvmStatic
         fun newInstance() = HoursFragment()
--- app/src/main/java/com/alexlyxy/weather/fragments/MainFragment.kt
+++ app/src/main/java/com/alexlyxy/weather/fragments/MainFragment.kt
@@ -2,6 +2,7 @@ package com.alexlyxy.weather.fragments
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,13 +11,24 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.activityViewModels
 import com.alexlyxy.weather.DaysFragment
 import com.alexlyxy.weather.HoursFragment
+import com.alexlyxy.weather.MainViewModel
 import com.alexlyxy.weather.R
 import com.alexlyxy.weather.adapters.VpAdapter
+import com.alexlyxy.weather.adapters.WeatherModel
 import com.alexlyxy.weather.databinding.FragmentMainBinding
 import com.alexlyxy.weather.isPermissionGranted
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.tabs.TabLayoutMediator
+import com.squareup.picasso.Picasso
+import org.json.JSONObject
+import java.util.Objects
+
+const val API_KEY = "8ea82f6e78674e4996e181556230908"
 
 class MainFragment : Fragment() {
     private val fList = listOf(
@@ -28,43 +40,136 @@ class MainFragment : Fragment() {
         "Days"
     )
 
-    private lateinit var pLauncher :ActivityResultLauncher<String>
-    private lateinit var binding :FragmentMainBinding
+    private lateinit var pLauncher: ActivityResultLauncher<String>
+    private lateinit var binding: FragmentMainBinding
+    private val  model : MainViewModel by activityViewModels()
 
     override fun onCreateView(
-        inflater :LayoutInflater,container :ViewGroup?,
-        savedInstanceState :Bundle?,
-    ) :View? {
-        binding = FragmentMainBinding.inflate(inflater,container,false)
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onViewCreated(view :View,savedInstanceState :Bundle?) {
-        super.onViewCreated(view,savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         checkPermission()
         init()
+        updateCurrentCard()
+        requestWeatherData("Kiev")
     }
 
     private fun init() = with(binding) {
         val adapter = VpAdapter(activity as FragmentActivity, fList)
         vp.adapter = adapter
-        TabLayoutMediator(tabLayout, vp) {
-            tab, pos -> tab.text = tList[pos]
+        TabLayoutMediator(tabLayout, vp) { tab, pos ->
+            tab.text = tList[pos]
         }.attach()
     }
 
-    private fun permissionListener(){
+    private fun updateCurrentCard() = with(binding){
+        model.liveDataCurrent.observe(viewLifecycleOwner){
+            val  maxMinTemp = "${it.maxTemp}°С / ${it.minTemp}°С"
+            tvData.text = it.time
+            tvCity.text= it.city
+            tvCurrentTemp.text= it.currentTemp
+            tvCondition.text= it.condition
+            tvMaxMin.text = maxMinTemp
+            Picasso.get().load("https:" + it.imageURL).into(imWeather)
+        }
+    }
+
+    private fun permissionListener() {
         pLauncher = registerForActivityResult(
-            ActivityResultContracts.RequestPermission()){
+            ActivityResultContracts.RequestPermission()
+        ) {
             Toast.makeText(activity, "permission is $it", Toast.LENGTH_LONG).show()
         }
     }
 
-    private fun checkPermission(){
-        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+    private fun checkPermission() {
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
             permissionListener()
             pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
+    }
+
+    private fun requestWeatherData(city: String) {
+        val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
+                API_KEY +
+                "&q=" +
+                city +
+                "&days=" +
+                "3" +
+                "&aqi=no&alerts=no"
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            //{ result -> Log.d("MyLog", "Result: $result") },
+            { result ->
+                parseWeatherData(result)
+            },
+            { error ->
+                Log.d("MyLog", "Error: $error")
+            }
+        )
+        queue.add(request)
+    }
+
+    private fun parseWeatherData(result: String) {
+        val mainObject = JSONObject(result)
+        val  list = parseDays(mainObject)
+        parseCurrentData(mainObject, list[0])
+    }
+
+    private fun parseDays(mainObject: JSONObject): List<WeatherModel> {
+        val list = ArrayList<WeatherModel>()
+        val daysArray = mainObject.getJSONObject("forecast")
+            .getJSONArray("forecastday")
+        val name = mainObject.getJSONObject("location").getString("name");
+        for (i in 0 until daysArray.length()) {
+            val day = daysArray[i] as JSONObject
+            val item = WeatherModel(
+                name,
+                day.getString("date"),
+                day.getJSONObject("day").getJSONObject("condition")
+                    .getString("text"),
+                currentTemp = "",
+                day.getJSONObject("day").getString("maxtemp_c"),
+                day.getJSONObject("day").getString("mintemp_c"),
+                day.getJSONObject("day").getJSONObject("condition")
+                    .getString("icon"),
+                day.getJSONArray("hour").toString()
+            )
+            list.add(item)
+        }
+        return list
+    }
+
+    private fun parseCurrentData(mainObject: JSONObject, weatherItem : WeatherModel) {
+        val item = WeatherModel(
+            mainObject.getJSONObject("location").getString("name"),
+            mainObject.getJSONObject("current").getString("last_updated"),
+            mainObject.getJSONObject("current")
+                .getJSONObject("condition").getString("text"),
+            mainObject.getJSONObject("current").getString("temp_c"),
+            weatherItem.maxTemp,
+            weatherItem.minTemp,
+            mainObject.getJSONObject("current").getJSONObject("condition").getString("icon"),
+            weatherItem.hours
+        )
+        model.liveDataCurrent.value = item
+
+        Log.d("MyLog", "City: ${item.city}")
+        Log.d("MyLog", "Time: ${item.time}")
+        Log.d("MyLog", "Condition: ${item.condition}")
+        Log.d("MyLog", "Temp: ${item.currentTemp}")
+        Log.d("MyLog", "MaxTemp: ${item.maxTemp}")
+        Log.d("MyLog", "MinTemp: ${item.minTemp}")
+        Log.d("MyLog", "Url: ${item.imageURL}")
+        Log.d("MyLog", "Hours: ${item.hours}")
     }
 
     companion object {
